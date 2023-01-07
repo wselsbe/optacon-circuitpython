0eda1f4d4fd5aea6d30ece2b7d7f740072ac2d0e
Wannes Sels <wannes@wsels.be>
2023-01-07 20:01:29 +0100

    drv2665 cleanup

--- drv2665.py
+++ drv2665.py
@@ -2,50 +2,53 @@
 from adafruit_bus_device import i2c_device
 from busio import I2C
 
+
+DRV2665_ADDR = 0x59
+
+#register 0
+_REGISTER_0 = 0x00
+_FIFO_EMPTY_MASK = 0b00000010
+_FIFO_FULL_MASK = 0b00000001
+
+#register 1
+_REGISTER_1 = 0x01
+_CHIPID_MASK = 0b01111000
+
+_INPUT_MASK = 0b00000100
+INPUT_DIGITAL = 0 << 2
+INPUT_ANALOG = 1 << 2
+
+_GAIN_MASK = 0b00000011
 GAIN_25V = 0
 GAIN_50V = 1
 GAIN_75V = 2
 GAIN_100V = 3
-_GAIN_MASK = 0b00000011
 
-INPUT_DIGITAL = 0 << 2
-INPUT_ANALOG = 1 << 2
-_INPUT_MASK = 0b00000100
+#register 2
+_REGISTER_2 = 0x02
+_RESET = 1 << 7
 
+_STANDBY_MASK = 0b01000000
 _STANDBY_FALSE = 0 << 6
 _STANDBY_TRUE = 1 << 6
-_STANDBY_MASK = 0b01000000
 
+_TIMEOUT_MASK = 0b00001100
 TIMEOUT_5MS = 0 << 2
 TIMEOUT_10MS = 1 << 2
 TIMEOUT_15MS = 2 << 2
 TIMEOUT_20MS = 3 << 2
-_TIMEOUT_MASK = 0b00001100
 
+_ENABLE_MASK = 0b00000010
 ENABLE_AUTO = 0 << 1
 ENABLE_OVERRIDE = 1 << 1
-_ENABLE_MASK = 0b00000010
 
-_FIFO_EMPTY_MASK = 0b00000010
-_FIFO_FULL_MASK = 0b00000001
-
-RESET = 1 << 7
-
-DRV2665_ADDR = 0x59
-
-REGISTER_0 = 0x00
-REGISTER_1 = 0x01
-REGISTER_2 = 0x02
-REGISTER_DATA = 0x0B
-
-_CHIPID_MASK = 0b01111000
+#register fifo queue for data
+_REGISTER_DATA = 0x0B
 
 
-
-class Driver:
-    # Class-level buffer for reading and writing data with the sensor.
-    # This reduces memory allocations but means the code is not re-entrant or
-    # thread safe!
+class DRV2665:
+    # Class-level buffer for reading and writing data.
+    # This reduces memory allocations but means the code is not re-entrant or thread safe!
     _BUFFER = bytearray(2)
 
     def __init__(self, i2c: I2C, address: int = DRV2665_ADDR) -> None:
@@ -58,10 +61,10 @@ class Driver:
         self._read_register1()
         chipid = (self._register1_value & _CHIPID_MASK) >> 3
         if chipid not in (5,7):
-            raise RuntimeError("Failed to find DRV2665, check wiring!")
+            raise RuntimeError("Failed to find DRV2665 or DRV2667, check wiring!")
 
     def reset(self):
-        self._write_u8(REGISTER_2, RESET)
+        self._write_u8(_REGISTER_2, _RESET)
         self._read_register1()
         self._read_register2()    
 
@@ -124,43 +127,41 @@ class Driver:
         self._register2_value &= ~_ENABLE_MASK
         self._register2_value |= value
         self._write_register2()
-
     
     @property
     def queue_empty_full(self) -> tuple[bool, bool]:
-        register0_value = self._read_u8(REGISTER_0)
-        return register0_value & _FIFO_EMPTY_MASK, register0_value & _FIFO_FULL_MASK
+        register0_value = self._read_u8(_REGISTER_0)
+        return register0_value & _FIFO_EMPTY_MASK == _FIFO_EMPTY_MASK, register0_value & _FIFO_FULL_MASK == _FIFO_FULL_MASK
 
     def write_digital(self, value: int):
-        self._write_s8(REGISTER_DATA, value)
+        self._write_s8(_REGISTER_DATA, value)
 
     def _read_register1(self):
-        self._register1_value = self._read_u8(REGISTER_1)
+        self._register1_value = self._read_u8(_REGISTER_1)
         
     def _write_register1(self):
-        self._write_u8(REGISTER_1, self._register1_value)    
+        self._write_u8(_REGISTER_1, self._register1_value)    
     
     def _read_register2(self):
-        self._register2_value = self._read_u8(REGISTER_2)
+        self._register2_value = self._read_u8(_REGISTER_2)
 
     def _write_register2(self):
-        self._write_u8(REGISTER_2, self._register2_value)        
+        self._write_u8(_REGISTER_2, self._register2_value)        
 
     def _read_u8(self, address: int) -> int:
-        # Read an 8-bit unsigned value from the specified 8-bit address.
         with self._device as i2c:
             self._BUFFER[0] = address & 0xFF
             i2c.write_then_readinto(self._BUFFER, self._BUFFER, out_end=1, in_end=1)
         return self._BUFFER[0]
 
-    def _write_u8(self, address: int, val: int) -> None:
+    def _write_u8(self, address: int, value: int) -> None:
         with self._device as i2c:
             self._BUFFER[0] = address & 0xFF
-            self._BUFFER[1] = val & 0xFF
+            self._BUFFER[1] = value & 0xFF
             i2c.write(self._BUFFER, end=2)
 
-    def _write_s8(self, address: int, val: int) -> None:        
-        assert -127 < val < 128
-        if val < 0:
-            val += 0xFF # two's complement
-        self._write_u8(address, val)
+    def _write_s8(self, address: int, value: int) -> None:        
+        assert -128 <= value <= 127
+        if value < 0:
+            value += 0xFF # two's complement
+        self._write_u8(address, value)
