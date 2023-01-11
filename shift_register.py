34ee133861060aba548267863697a67343689c78
Wannes Sels <wannes@wsels.be>
2023-01-11 15:51:58 +0100

    fix spi speed, pzt_common mask

--- shift_register.py
+++ shift_register.py
@@ -5,53 +5,78 @@ from adafruit_bus_device.spi_device import SPIDevice
 _MASK_COMMON_U1 = 0b00111111
 _MASK_COMMON_U2 = 0b11111100
 
-_PINS_ALL = bytes([0xFF & ~_MASK_COMMON_U1, 0xFF, 0xFF, 0xFF & ~_MASK_COMMON_U2])
+_PINS_ALL = bytes([0xFF, 0xFF & ~_MASK_COMMON_U1, 0xFF & ~_MASK_COMMON_U2, 0xFF])
 _PINS_NONE = bytes([0,0,0,0])
 
 class ShiftRegister:
     
     _device: SPIDevice
     _data: bytearray
+    _read_buffer: bytearray
 
     def __init__(self,
         spi: SPI,
         latch: DigitalInOut,
         polarity: DigitalInOut,        
-        baudrate: int = 1000000
+        baudrate: int = 250 * 1000
     ) -> None:
         self._device = SPIDevice(spi, latch, baudrate=baudrate)
         self._data = bytearray(4)
+        self._read_buffer = bytearray(4)
 
         self._polarity = polarity
-        self._polarity.value = False
+        self._polarity.value = True
 
-        self._validate()
+        #self._validate()
 
     def _validate(self):
         with self._device as spi:
             spi.write(_PINS_NONE)
-            spi.write_readinto(_PINS_ALL, self._data)
-            assert self._data[0] == _PINS_NONE[0] and self._data[1] == _PINS_NONE[1] and self._data[2] == _PINS_NONE[2] and self._data[3] == _PINS_NONE[3]
-            spi.write_readinto(_PINS_NONE, self._data)
-            assert self._data[0] == _PINS_ALL[0] and self._data[1] == _PINS_ALL[1] and self._data[2] == _PINS_ALL[2] and self._data[3] == _PINS_ALL[3]
-            spi.write_readinto(_PINS_NONE, self._data)
-            assert self._data[0] == _PINS_NONE[0] and self._data[1] == _PINS_NONE[1] and self._data[2] == _PINS_NONE[2] and self._data[3] == _PINS_NONE[3]
+            spi.write_readinto(_PINS_ALL, self._read_buffer)
+            assert self._read_buffer[0] == _PINS_NONE[0] and self._read_buffer[1] == _PINS_NONE[1] and self._read_buffer[2] == _PINS_NONE[2] and self._read_buffer[3] == _PINS_NONE[3]
+            spi.write_readinto(_PINS_NONE, self._read_buffer)
+            assert self._read_buffer[0] == _PINS_ALL[0] and self._read_buffer[1] == _PINS_ALL[1] and self._read_buffer[2] == _PINS_ALL[2] and self._read_buffer[3] == _PINS_ALL[3]
+            spi.write_readinto(_PINS_NONE, self._read_buffer)
+            assert self._read_buffer[0] == _PINS_NONE[0] and self._read_buffer[1] == _PINS_NONE[1] and self._read_buffer[2] == _PINS_NONE[2] and self._read_buffer[3] == _PINS_NONE[3]
 
     def toggle_polarity(self):
-        self.polarity.value = not self.polarity.value
+        self._polarity.value = not self._polarity.value
 
     def set_pin(self, pin: int, value: bool):
         assert 1 <= pin <= 20
-        shift_pos = pin - 1 + 6  # from 1-based to 0-based, skip PZT_Common
-        byte_index = shift_pos // 8
-        byte_mask = 1 << (shift_pos % 8)
+        if pin <= 2:
+            byte_index = 1
+            byte_mask = 1 << (pin+5)
+        elif pin <= 10:
+            byte_index = 0
+            byte_mask = 1 << (pin-3)
+        elif pin <= 18:
+            byte_index = 3
+            byte_mask = 1 << (pin-11)
+        else:
+            byte_index = 2
+            byte_mask = 1 << (pin-19)
+            
         if value:
             self._data[byte_index] |= byte_mask
         else:
             self._data[byte_index] &= ~byte_mask
 
     def write(self):
-        self._data[0] &= ~_MASK_COMMON_U1  # PZT_Common always 0
-        self._data[3] &= ~_MASK_COMMON_U2
+        self._data[1] &= ~_MASK_COMMON_U1  # PZT_Common always 0
+        self._data[2] &= ~_MASK_COMMON_U2
+        self._write_verify()
+
+    def _write_verify(self):        
+        print(f"writing spi {self._data[0]:#010b} {self._data[1]:#010b} {self._data[2]:#010b} {self._data[3]:#010b}")
         with self._device as spi:
             spi.write(self._data)
+            spi.write_readinto(self._data, self._read_buffer)
+            try:
+                assert self._data[0] == self._read_buffer[0]
+                assert self._data[1] == self._read_buffer[1]
+                assert self._data[2] == self._read_buffer[2]
+                assert self._data[3] == self._read_buffer[3]
+            except AssertionError:
+                spi.write(_PINS_NONE)  # all outputs off to avoid shorts on PZT_Common
+                raise
