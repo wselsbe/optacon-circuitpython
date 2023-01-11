83521b73c0758140a015d3747893bd27cff6d42b
Wannes Sels <wannes@wsels.be>
2023-01-11 15:52:30 +0100

    drv reset change defaults

--- code.py
+++ code.py
@@ -12,7 +12,7 @@ shift_register: ShiftRegister
 def get_shift_register() -> ShiftRegister:
     spi = SPI(clock = board.GP6, MOSI=board.GP7, MISO=board.GP4)
 
-    latch = DigitalInOut(board.GP11)
+    latch = DigitalInOut(board.GP5)
     latch.switch_to_output()
 
     polarity=DigitalInOut(board.GP10)
@@ -28,6 +28,10 @@ def get_drv() -> DRV2665:
         print("DRV2665 found")
         reset_drv(i2c)
         drv = DRV2665(i2c=i2c)
+        
+        drv.standby=False
+        drv.input=INPUT_ANALOG
+        drv.gain=GAIN_100V
         #drv.reset()
         return drv
     else:
@@ -60,12 +64,25 @@ def main():
     global drv, shift_register
     drv = get_drv()
     shift_register = get_shift_register()
+    
+    #shift_register.set_pin(1, True)
+    #shift_register.set_pin(2, True)
+    #shift_register.set_pin(3, True)
+    #shift_register.set_pin(4, True)
+    #shift_register.set_pin(5, True)
+    #shift_register.set_pin(6, True)
+    #shift_register.set_pin(7, True)
+    #shift_register.set_pin(8, True)
+    #shift_register.set_pin(9, True)
+    #shift_register.set_pin(10, True)
+    #shift_register.write()
+
 
     drv.standby=False
     drv.input=INPUT_ANALOG
     drv.gain=GAIN_100V
     drv.timeout=TIMEOUT_20MS
-    drv.enable=ENABLE_OVERRIDE
+    #drv.enable=ENABLE_OVERRIDE
 
 if __name__ == "__main__":
     main()
